5c2909eccf892772cbd5f783dee5dc673a2eb9b8
Paul H <dovernh@gmail.com>
2026-06-26 18:20:19 -0400

    use old name, render does not use new name

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // The deployed Render backend URL.
-  apiUrl: 'https://saas-issue-tracker-be-nci2.onrender.com',
+  apiUrl: 'https://issue-tracker-be-nci2.onrender.com',
   // Sentry frontend DSN (public).
   sentryDsn:
     'https://e6bf14e94ea5e86c11d2498c9bdd1d7b@o4509482618388480.ingest.us.sentry.io/4511588295966720',
